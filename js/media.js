ca4d993d8d639ee5823fe9cc00449b3b44b958ca
Omniabene <omniabene2@mail.ru>
2023-11-14 22:08:38 +0300

    photo4

--- js/media.js
+++ js/media.js
@@ -12,7 +12,7 @@ const audioVisualizer = document.getElementById('audioVisualizer');
 const ctx = audioVisualizer.getContext('2d');
 let playyy = false;
 
-const songs = ['Котик', 'Когда я стану кошкой', 'Котозависимый', 'Чёрный кот'];
+const songs = ['Котозависимый', 'Когда я стану кошкой', 'Котик', 'Чёрный кот'];
 let songIndex = 0;
 
 function loadSong(song) {
